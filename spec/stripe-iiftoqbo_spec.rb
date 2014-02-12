402fd9fcdde8f5a8fc59eb89d793c0988428ff1f
Gilman Tolle <gilman.tolle@gmail.com>
2014-02-12 09:51:08 -0800

    Added sample data files, and end-to-end rspecs.

--- spec/stripe-iiftoqbo_spec.rb
+++ spec/stripe-iiftoqbo_spec.rb
@@ -1,21 +1,9 @@
 describe StripeIIFToQBO do
-  empty_qbo = <<EOS
-OFXHEADER:100
-DATA:OFXSGML
-VERSION:103
-SECURITY:NONE
-ENCODING:USASCII
-CHARSET:1252
-COMPRESSION:NONE
-OLDFILEUID:NONE
-NEWFILEUID:NONE
-
-<OFX><SIGNONMSGSRSV1><SONRS><STATUS><CODE>0</CODE><SEVERITY>INFO</SEVERITY></STATUS><DTSERVER>20140211000000</DTSERVER><LANGUAGE>ENG</LANGUAGE><FI><ORG>Stripe</ORG><FID>0</FID></FI><INTU.BID>0</INTU.BID></SONRS></SIGNONMSGSRSV1><BANKMSGSRSV1><STMTTRNRS><TRNUID>0</TRNUID><STATUS><CODE>0</CODE><SEVERITY>INFO</SEVERITY></STATUS><STMTRS><CURDEF>USD</CURDEF><BANKACCTFROM><BANKID>123456789</BANKID><ACCTID/><ACCTTYPE>CHECKING</ACCTTYPE></BANKACCTFROM><BANKTRANLIST/><LEDGERBAL><BALAMT>0.0</BALAMT></LEDGERBAL></STMTRS></STMTTRNRS></BANKMSGSRSV1></OFX>
-EOS
-
-  empty_csv = <<EOS
-Date,Name,Account,Memo,Amount
-EOS
+  empty_qbo = File.read(File.dirname(__FILE__) + "/sample-data/empty.qbo")
+  empty_csv = File.read(File.dirname(__FILE__) + "/sample-data/empty.csv")
+  basic_iif = File.read(File.dirname(__FILE__) + "/sample-data/basic.iif")
+  basic_qbo = File.read(File.dirname(__FILE__) + "/sample-data/basic.qbo")
+  basic_csv = File.read(File.dirname(__FILE__) + "/sample-data/basic.csv")
 
   it "should create an empty QBO file" do
     iiftoqbo = StripeIIFToQBO::Converter.new( :server_time => Date.new(2014,2,11) )
@@ -27,5 +15,17 @@ EOS
     iiftoqbo = StripeIIFToQBO::Converter.new( :server_time => Date.new(2014,2,11) )
     csv = iiftoqbo.to_csv
     expect( csv ).to eq(empty_csv)
+  end
+
+  it "should create a basic QBO file" do
+    iiftoqbo = StripeIIFToQBO::Converter.new( :server_time => Date.new(2014,2,11), :iif_file => basic_iif )
+    qbo = iiftoqbo.to_qbo
+    expect( qbo ).to eq(basic_qbo)
+  end
+
+  it "should create a basic CSV file" do
+    iiftoqbo = StripeIIFToQBO::Converter.new( :server_time => Date.new(2014,2,11), :iif_file => basic_iif )
+    csv = iiftoqbo.to_csv
+    expect( csv ).to eq(basic_csv)
   end
 end
