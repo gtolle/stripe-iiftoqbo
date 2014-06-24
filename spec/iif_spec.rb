2aa96fb815d7b3d6dba3b72037c7547a36510b87
Gilman Tolle <gilman.tolle@gmail.com>
2014-06-24 15:02:45 -0700

    Updated rspecs to match IIF format from Stripe as of June 24, 2014.

--- spec/iif_spec.rb
+++ spec/iif_spec.rb
@@ -10,34 +10,46 @@ describe IIF do
 
   it "should parse basic IIF files" do
     iif = IIF( basic_iif )
-    expect( iif.transactions.length ).to eq(7)
-    first_transaction = iif.transactions.first
-    expect( first_transaction.entries.length ).to eq(3)
+    expect( iif.transactions.length ).to eq(8)
+    first_transaction = iif.transactions[0]
+    expect( first_transaction.entries.length ).to eq(2)
     first_entry = first_transaction.entries.first
 
     expect( first_entry.type ).to eq("TRNS")
-    expect( first_entry.trnstype ).to eq("PAYOUT (TEST USER)")
+    expect( first_entry.trnstype ).to eq("CHECK")
     expect( first_entry.date ).to eq(Date.new(2014,1,23))
-    expect( first_entry.accnt ).to eq("Third-party Account")
-    expect( first_entry.amount ).to eq(135.15)
+    expect( first_entry.accnt ).to eq("Stripe Account")
+    expect( first_entry.amount ).to eq(-135.15)
     expect( first_entry.memo ).to eq("Transfer from Stripe: tr_3MCmCnHFyYXFB5")
 
     second_entry = first_transaction.entries[1]
 
     expect( second_entry.type ).to eq("SPL")
-    expect( second_entry.trnstype ).to eq("PAYOUT (TEST USER)")
+    expect( second_entry.trnstype ).to eq("CHECK")
     expect( second_entry.date ).to eq(Date.new(2014,1,23))
-    expect( second_entry.accnt ).to eq("Stripe Account")
-    expect( second_entry.amount ).to eq(-135.40)
+    expect( second_entry.accnt ).to eq("Stripe Third-party Account")
+    expect( second_entry.amount ).to eq(135.15)
     expect( second_entry.memo ).to eq("Transfer from Stripe: tr_3MCmCnHFyYXFB5")
+    expect( second_entry.name ).to eq("Nicole Chiu-Wang")
 
-    third_entry = first_transaction.entries[2]
+    second_transaction = iif.transactions[1]
 
-    expect( third_entry.type ).to eq("SPL")
-    expect( third_entry.trnstype ).to eq("GENERAL JOURNAL")
-    expect( third_entry.date ).to eq(Date.new(2014,1,23))
-    expect( third_entry.accnt ).to eq("Stripe Payment Processing Fees")
-    expect( third_entry.amount ).to eq(0.25)
-    expect( third_entry.memo ).to eq("Fees for transfer ID: tr_3MCmCnHFyYXFB5")
+    first_entry = second_transaction.entries[0]
+
+    expect( first_entry.type ).to eq("TRNS")
+    expect( first_entry.trnstype ).to eq("GENERAL JOURNAL")
+    expect( first_entry.date ).to eq(Date.new(2014,1,23))
+    expect( first_entry.accnt ).to eq("Stripe Account")
+    expect( first_entry.amount ).to eq(-0.25)
+    expect( first_entry.memo ).to eq("Fees for Transfer from Stripe: tr_3MCmCnHFyYXFB5")
+
+    second_entry = second_transaction.entries[1]
+
+    expect( second_entry.type ).to eq("SPL")
+    expect( second_entry.trnstype ).to eq("GENERAL JOURNAL")
+    expect( second_entry.date ).to eq(Date.new(2014,1,23))
+    expect( second_entry.accnt ).to eq("Stripe Payment Processing Fees")
+    expect( second_entry.amount ).to eq(0.25)
+    expect( second_entry.memo ).to eq("Fees for Transfer from Stripe: tr_3MCmCnHFyYXFB5")
   end
 end
